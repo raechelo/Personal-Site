d9b454fe3714c2d50654fced1b4179b0d8eb4f2a
raechel <odom.raechel@gmail.com>
2019-03-29 15:19:58 -0600

    initial commit

---
+++ src/Components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return(
+    <header class="Header">
+      <nav>
+        <a href="">About</a>
+        <a href="">Projects</a>
+        <a href="">Contact</a>
+      </nav>
+      <h1>raechel odom.</h1>
+    </header>
+  )
+}
+
+export default Header;
